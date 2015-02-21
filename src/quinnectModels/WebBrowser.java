52a032e4bc97f1646d50cd14658008efad6a6d8c
MIGALIZA <MIGALIZA@MIGALIZA-HP>
2015-02-21 15:06:39 +0000

    my first commit

---
+++ src/quinnectModels/WebBrowser.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quinnectModels;
+
+/**
+ *
+ * @author MIGALIZA
+ */
+public class WebBrowser {
+    private String siteName;
+    private String searchName;
+    
+    public WebBrowser(String site, String search){
+        site = siteName;
+        search = siteName;
+    }
+    
+    
+}
